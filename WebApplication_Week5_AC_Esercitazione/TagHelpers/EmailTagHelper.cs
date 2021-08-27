224fa28c2a5422038fcb6eb6dd381a89b5e48f7f
Eleonora Lombardo (c) <eleonora.lombardo@avanade.com>
2021-08-27 14:56:17 +0200

    Email

--- WebApplication_Week5_AC_Esercitazione/TagHelpers/EmailTagHelper.cs
+++ WebApplication_Week5_AC_Esercitazione/TagHelpers/EmailTagHelper.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace WebApplication.TagHelpers
 {
-    [HtmlTargetElement("e-mail")]
+    [HtmlTargetElement("e-mail", TagStructure = TagStructure.WithoutEndTag)]
     public class EmailTagHelper : TagHelper
     {
-        public string To { get; set; }
+        public string MailTo { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             output.TagMode = TagMode.StartTagAndEndTag;
-            output.Content.SetContent("Contact us!");
+            output.Content.SetContent("Contattaci via e-mail!");
             output.Attributes.SetAttribute("class", "btn btn-primaryMine");
-            output.Attributes.SetAttribute("href", $"mailto:{To}@avanade.com");
+            output.Attributes.SetAttribute("href", $"mailto:{MailTo}@avanade.com");
         }
     }
 }
